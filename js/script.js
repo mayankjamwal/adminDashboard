ee25f97e5529e2863886fd054766ab4f309b5a3e
Mayank Jamwal <jamwalmayank75@gmail.com>
2023-03-15 04:43:46 +0530

    Added new section element and styles

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 toggleButton.addEventListener('click',()=>{
     navbarLinks.classList.toggle('active');
 });
+
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         let scroll = $(window).scrollTop();
+//         console.log(scroll);
+//         if (scroll > 200) {
+//           $(".nav-head").css("opacity","0.9");
+//         }
+  
+//         else{
+//             $(".nav-head").css("opacity","1");  	
+//         }
+//     })
+//   })
